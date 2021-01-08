e57205ed7918e7f3aa75af1e8f39f2b95af8e359
leylatuon <leylatuon@users.noreply.github.com>
2021-01-08 10:29:06 -0500

    added all files

---
+++ public/sketch.js
@@ -0,0 +1,68 @@
+const {text} = require("express");
+
+// Keep track of our socket connection
+var socket;
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+  // Start a socket connection to the server
+  // Some day we would run this server somewhere else
+  socket = io.connect('http://localhost:3000');
+  // We make a named event called 'mouse' and write an
+  // anonymous callback function
+  socket.on('mouse',
+    // When we receive data
+    function(data) {
+      console.log("Got: " + data.x + " " + data.y);
+      // Draw a blue circle
+      fill(0,0,255);
+      noStroke();
+      ellipse(data.x, data.y, 20, 20);
+    }
+  );
+  socket.on('button',
+  // When we receive data
+  function() {
+    console.log("Got: button pressed");
+    //textSize(32);
+    //fill(0, 102, 153);
+    //text('NEXT SLIDE PLEASE',100,100);
+    document.getElementById("nextslide").innerHTML = "NEXT SLIDE PLEASE"
+  }
+);
+}
+
+function draw() {
+  // Nothing
+}
+
+function mouseDragged() {
+  // Draw some white circles
+  fill(255);
+  noStroke();
+  ellipse(mouseX,mouseY,20,20);
+  // Send the mouse coordinates
+  sendmouse(mouseX,mouseY);
+}
+
+// Function for sending to the socket
+function sendmouse(xpos, ypos) {
+  // We are sending!
+  console.log("sendmouse: " + xpos + " " + ypos);
+  
+  // Make a little object with  and y
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+
+  // Send that object to the socket
+  socket.emit('mouse',data);
+}
+
+function sendbutton()
+{
+    console.log("sending button");
+    socket.emit('button')
+}
